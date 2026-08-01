c789e1481e5c19eccafd5306110f5bd405bf4b06
Pepper Gray <hello@peppergray.xyz>
2026-08-01 22:05:47 +0200

    probe if crt0 embedds a PE header
    fall back to local crt0 if objcopy cannot create pefiles
    and system crt0 does not embedd a PE header
    
    Signed-off-by: Pepper Gray <hello@peppergray.xyz>

--- efi/probe-objcopy-efi.py
+++ efi/probe-objcopy-efi.py
@@ -5,39 +5,69 @@ import sys
 import argparse
 import tempfile
 import pathlib
+import pefile
+
 
 def _probe_objcopy(args) -> int:
 
     with tempfile.TemporaryDirectory(prefix="objcopy-efi-probe-") as tmp:
         tmpdir = pathlib.Path(tmp)
+
+        obj_file = tmpdir / "probe.o"
         elf_file = tmpdir / "probe.a"
         efi_file = tmpdir / "probe.efi"
 
-        cmd = (
-            args.cc.split()
-            + [
-                "-x", "c",
-                "-nostdlib",
-                "-fPIC",
-                "-shared",
-                "-Wl,-e,_start",
-                "-o", str(elf_file),
-                "-",
-            ]
-        )
-        subprocess.run(cmd, input=b"void _start(void) {}\n", check=True)
+        efi_src = b"""
+            typedef __UINTPTR_TYPE__ EFI_STATUS;
+            EFI_STATUS _relocate(void *base, void *dyn, void *image, void *systab) { return 0; }
+            EFI_STATUS efi_main(void *image, void *systab) { return 0; }
+            """
 
-        cmd = (
-            args.objcopy.split()
-            + [
-                "-O", args.target,
-                str(elf_file),
-                str(efi_file),
-            ]
-        )
-        objcopy_result = subprocess.run(cmd, check=False)
+        cc_cmd = args.cc.split() + [
+            "-x",
+            "c",
+            "-c",
+            "-fno-stack-protector",
+            "-fPIC",
+            "-o",
+            str(obj_file),
+            "-",
+        ]
+        subprocess.run(cc_cmd, input=efi_src, check=True)
 
-        return objcopy_result.returncode
+        ld_cmd = args.cc.split() + [
+            "-nostdlib",
+            "-shared",
+            "-Wl,-Bsymbolic",
+            "-Wl,-znocombreloc",
+            "-Wl,-e,_start",
+            f"-Wl,-T,{args.lds}",
+            str(args.crt0),
+            str(obj_file),
+            "-o",
+            str(elf_file),
+        ]
+        subprocess.run(ld_cmd, check=True)
+
+        objcopy_cmd = args.objcopy.split() + [
+            "-O",
+            args.target,
+            str(elf_file),
+            str(efi_file),
+        ]
+
+        result = subprocess.run(objcopy_cmd, check=False)
+        if result.returncode != 0:
+            return result.returncode
+
+        try:
+            pe = pefile.PE(efi_file)
+        except pefile.PEFormatError as error:
+            print(f"invalid EFI PE image: {error}", file=sys.stderr)
+            return 1
+
+        return 0
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -48,6 +78,8 @@ if __name__ == "__main__":
         "--objcopy", default="objcopy", help="Binary file to use for objcopy"
     )
     parser.add_argument("--target", default="efi-app-x86_64", help="EFI output target")
+    parser.add_argument("--lds", default="efi.lds", help="EFI linker script")
+    parser.add_argument("--crt0", default="crt0.o", help="EFI C runtime initialization")
 
     _args = parser.parse_args()
     res = _probe_objcopy(_args)
